96ff86c85133c1df0af9d74c4911730d75a3f4cc
Niyonkuru Elisa <niyonkuruelisa@gmail.com>
2021-01-09 12:26:19 +0200

    Removed password_digest from User Object When Authenticating

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::AuthenticationController < ApplicationController
       render json: {
           success: true,
           token: command.result,
-          user: user
+          user: user.as_json(:except => (:password_digest))
       }
     else
       render json: {success: false, error: command.errors}, status: :ok
