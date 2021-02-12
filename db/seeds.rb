93dc491b85176adb9e99aca0f4028b3a3a536551
Niyonkuru Elisa <niyonkuruelisa@gmail.com>
2021-02-12 16:34:07 +0200

    Added system Info SEED

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@
 # Examples:
 #
 # movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-User.create!(first_name: 'Niyonkuru',last_name: 'Elisa',email: 'niyonkuruelisa@gmail.com',phone_number:'+250785290539',role: 'admin',password: 'Test@afrodrama.com')
+User.create!(first_name: 'Niyonkuru',last_name: 'Elisa',email: 'niyonkuruelisa2@gmail.com',phone_number:'+250785290539',role: 'admin',password: 'Test@afrodrama.com')
 User.create!(first_name: 'Mugenimana',last_name: 'fifi',email: 'fifi@gmail.com',phone_number:'+250785290539',password: 'Test@frodrama.com')
 User.create!(first_name: 'Test',last_name: 'Client',email: 'test@gmail.com',phone_number:'+250785290539',role: 'tester',password: 'Test@afrodrama.com')
 System.create!(title: 'Afrodrama', company_name: 'Afrodrama LTD', about: 'Movies & Documentaries Streaming Platform', slogan: 'Design solution for any media', email: 'afrodrama@gmail.com', phone1: '+250785290539', phone2: '', keywords: 'Movie,ShortM Movies,Stream,ZacuTv', address: 'Kigali RW')
