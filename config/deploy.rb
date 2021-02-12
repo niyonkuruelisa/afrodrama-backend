652a622076011f34029c1179ea18ff40e886ada7
Niyonkuru Elisa <niyonkuruelisa@gmail.com>
2021-02-12 11:49:32 +0200

    Aded pty to capistrano settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock "~> 3.15.0"
 
 set :application, "api"
 set :repo_url, "git@github.com:niyonkuruelisa/afrodrama-backend.git"
-
+set :pty, true
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 set :branch, "main"
