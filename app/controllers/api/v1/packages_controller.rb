0beb0a8ae8e9c9877c0cec221579925ae87d765c
Niyonkuru Elisa <niyonkuruelisa@gmail.com>
2021-02-14 10:23:05 +0200

    Added JSON beautify

--- app/controllers/api/v1/packages_controller.rb
+++ app/controllers/api/v1/packages_controller.rb
@@ -12,14 +12,14 @@ class Api::V1::PackagesController < ApplicationController
   end
   # GET /packages/1
   def show
-    render json: @package
+    render json: {success: true,package: @packages}
   end
   # POST /packages
   def create
     @package = Package.new(package_params)
 
     if @package.save
-      render json: @package, status: :created
+      render json: {success: true,package: @package}, status: :created
     else
       render json: @package.errors, status: :unprocessable_entity
     end
@@ -28,7 +28,7 @@ class Api::V1::PackagesController < ApplicationController
   # PATCH/PUT /packages/1
   def update
     if @package.update(package_params)
-      render json: @package
+      render json: {success: true,package: @package}
     else
       render json: @package.errors, status: :unprocessable_entity
     end
