fef4fc196f128dfd51d5243479463d9ffaf72c10
Niyonkuru Elisa <niyonkuruelisa@gmail.com>
2021-02-19 19:04:30 +0200

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,29 +4,27 @@
 # Examples:
 #
 # movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-# default location for seeding users
-user_city      = "Kigali"
-user_region    = "Kigali"
-user_country   = "RW"
-user_timezone  = "Africa/Kigali"
-user_location  = "-1.9500,30.0588"
+
 # create default users
 User.create!(first_name: 'Niyonkuru',last_name: 'Elisa',email: 'niyonkuruelisa2@gmail.com',phone_number:'+250785290539',role: 'admin',password: 'Test@afrodrama.com',
-             user_city: user_city,
-             user_region: user_region,
-             user_country: user_country,
-             user_timezone: user_timezone,
-             user_location: user_location)
+             user_city: "Kigali",
+             user_region: "Kigali",
+             user_country: "RW",
+             user_timezone: "Africa/Kigali",
+             user_location: "-1.9500,30.0588")
 User.create!(first_name: 'Mugenimana',last_name: 'fifi',email: 'fifi@gmail.com',phone_number:'+250785290539',password: 'Test@afrodrama.com',
-             user_city: user_city,
-             user_region: user_region,
-             user_country: user_country,
-             user_timezone: user_timezone,
-             user_location: user_location)
+             user_city: "Kigali",
+             user_region: "Kigali",
+             user_country: "RW",
+             user_timezone: "Africa/Kigali",
+             user_location: "-1.9500,30.0588")
 User.create!(first_name: 'Test',last_name: 'Client',email: 'test@gmail.com',phone_number:'+250785290539',role: 'tester',password: 'Test@afrodrama.com',
-             user_city: user_city,
-             user_region: user_region,
-             user_country: user_country,
-             user_timezone: user_timezone,
-             user_location: user_location)
-System.create!(title: 'Afrodrama', company_name: 'Afrodrama LTD', about: 'Movies & Documentaries Streaming Platform', slogan: 'Design solution for any media', email: 'afrodrama@gmail.com', phone1: '+250785290539', phone2: '', keywords: 'Movie,ShortM Movies,Stream,ZacuTv', address: 'Kigali RW')
+             user_city: "Kigali",
+             user_region: "Kigali",
+             user_country: "RW",
+             user_timezone: "Africa/Kigali",
+             user_location: "-1.9500,30.0588")
+
+unless System.find_by_title("Afrodrama").present?
+  System.create!(title: 'Afrodrama', company_name: 'Afrodrama LTD', about: 'Movies & Documentaries Streaming Platform', slogan: 'Design solution for any media', email: 'afrodrama@gmail.com', phone1: '+250785290539', phone2: '', keywords: 'Movie,ShortM Movies,Stream,ZacuTv', address: 'Kigali RW')
+end
