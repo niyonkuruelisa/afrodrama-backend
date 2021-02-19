08323098c53c675cad591aa492625a8a728dc61f
Niyonkuru Elisa <niyonkuruelisa@gmail.com>
2021-02-19 17:11:37 +0200

    Test safe location

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::AuthenticationController < ApplicationController
     command = AuthenticateUser.call(params['email'],params['password'])
     if command.success?
       user = User.find_by_email(params['email'])
-      location = request.location
+      location = request.safe_location
       render json: {
           success: true,
           token: command.result,
