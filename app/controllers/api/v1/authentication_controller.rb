e53fdbb26fc1e87ad46bd5c7521d2561568240f7
Niyonkuru Elisa <niyonkuruelisa@gmail.com>
2021-02-21 14:36:35 +0200

    working on user location

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -12,10 +12,10 @@ class Api::V1::AuthenticationController < ApplicationController
       user_ip = request.remote_ip
       location = Geocoder.search(user_ip)[0]
       user_city      = location[]["data"]["city"].to_s
-      # user_region    = location["data"]["region"].to_s
-      # user_country   = location["data"]["country"].to_s
-      # user_timezone  = location["data"]["timezone"].to_s
-      # user_location  = location["data"]["loc"].to_s
+      user_region    = location[]["data"]["region"].to_s
+      user_country   = location[]["data"]["country"].to_s
+      user_timezone  = location[]["data"]["timezone"].to_s
+      user_location  = location[]["data"]["loc"].to_s
       render json: {
           success: true,
           token: command.result,
@@ -35,11 +35,13 @@ class Api::V1::AuthenticationController < ApplicationController
     # get user's location and ip for Timezone.
     user_ip = request.remote_ip
     location = Geocoder.search(user_ip)[0]
-    user_city      = location["data"]["city"].to_s
-    user_region    = location["data"]["region"].to_s
-    user_country   = location["data"]["country"].to_s
-    user_timezone  = location["data"]["timezone"].to_s
-    user_location  = location["data"]["loc"].to_s
+    
+    user_city      = location[]["data"]["city"].to_s
+    user_region    = location[]["data"]["region"].to_s
+    user_country   = location[]["data"]["country"].to_s
+    user_timezone  = location[]["data"]["timezone"].to_s
+    user_location  = location[]["data"]["loc"].to_s
+
     @user  = User.new(first_name: params['firstName'],
                       last_name: params['lastName'],
                       email: params['email'],
