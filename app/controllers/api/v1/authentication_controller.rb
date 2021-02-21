413916f68db56e4ac19a681b606f3fc0de154452
Niyonkuru Elisa <niyonkuruelisa@gmail.com>
2021-02-21 14:10:21 +0200

    working on user location

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -11,18 +11,12 @@ class Api::V1::AuthenticationController < ApplicationController
           # get user's location and ip for Timezone.
       user_ip = request.remote_ip
       location = Geocoder.search(user_ip)
-      user_city      = location[0]["data"]["city"].to_s
-      user_region    = location[0]["data"]["region"].to_s
-      user_country   = location[0]["data"]["country"].to_s
-      user_timezone  = location[0]["data"]["timezone"].to_s
-      user_location  = location[0]["data"]["loc"].to_s
 
       render json: {
           success: true,
           token: command.result,
           user: user.as_json(:except => (:password_digest)),
-          location: location,
-          location_test: user_city,
+          location: location
       }
     else
       render json: {success: false, error: command.errors}, status: :ok
